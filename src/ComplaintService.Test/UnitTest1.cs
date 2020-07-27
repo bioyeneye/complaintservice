6b54311408417824fa95260cbcd756c891da10fc
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-07-27 19:17:11 +0100

    created test

---
+++ src/ComplaintService.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ComplaintService.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Sample_Test()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
